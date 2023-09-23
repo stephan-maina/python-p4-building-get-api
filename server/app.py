eaeca59dec7edefb828e7c74a4adb799335e1668
Mr.Perfect 092 <135037455+stephan-maina@users.noreply.github.com>
2023-09-24 00:30:29 +0300

    completed.

--- server/app.py
+++ server/app.py
@@ -1,23 +1,22 @@
-#!/usr/bin/env python3
-
-from flask import Flask, jsonify, make_response
-from flask_sqlalchemy import SQLAlchemy
-from flask_migrate import Migrate
-
-from models import db, User, Review, Game
+from flask import Flask, jsonify
+from models import db, Book, Team
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-app.json.compact = False
-
-migrate = Migrate(app, db)
-
 db.init_app(app)
 
-@app.route('/')
-def index():
-    return "Index for Game/Review/User API"
+@app.route('/players', methods=['GET'])
+def get_players():
+    players = Book.query.all()
+    player_list = [player.to_dict() for player in players]
+    return jsonify(player_list)
+
+@app.route('/teams', methods=['GET'])
+def get_teams():
+    teams = Team.query.all()
+    team_list = [team.to_dict() for team in teams]
+    return jsonify(team_list)
 
 if __name__ == '__main__':
-    app.run(port=5555, debug=True)
+    app.run(debug=True)
