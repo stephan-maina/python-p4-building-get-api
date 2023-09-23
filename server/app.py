68c7e2065633bfe8263ff028df52e135365538fe
Mr.Perfect 092 <135037455+stephan-maina@users.noreply.github.com>
2023-09-24 00:34:30 +0300

    completed...

--- server/app.py
+++ server/app.py
@@ -1,22 +1,27 @@
-from flask import Flask, jsonify
-from models import db, Book, Team
+from app import db
+from models import Book, Team
 
-app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-db.init_app(app)
+# Create and add some initial data to the database
+def seed_data():
+    with db.app.app_context():
+        db.create_all()
 
-@app.route('/players', methods=['GET'])
-def get_players():
-    players = Book.query.all()
-    player_list = [player.to_dict() for player in players]
-    return jsonify(player_list)
+        # Add books (players)
+        book1 = Book(title='Cristiano Ronaldo', author='Al Nassr')
+        book2 = Book(title='Darwin Nunez', author='Liverpool FC')
+        book3 = Book(title='Valverde', author='Real Madrid')
 
-@app.route('/teams', methods=['GET'])
-def get_teams():
-    teams = Team.query.all()
-    team_list = [team.to_dict() for team in teams]
-    return jsonify(team_list)
+        db.session.add(book1)
+        db.session.add(book2)
+        db.session.add(book3)
 
-if __name__ == '__main__':
-    app.run(debug=True)
+        # Add teams
+        team1 = Team(name='Al Nassr', player='Cristiano Ronaldo')
+        team2 = Team(name='Liverpool FC', player='Darwin Nunez')
+        team3 = Team(name='Real Madrid', player='Valverde')
+
+        db.session.add(team1)
+        db.session.add(team2)
+        db.session.add(team3)
+
+        db.session.commit()
